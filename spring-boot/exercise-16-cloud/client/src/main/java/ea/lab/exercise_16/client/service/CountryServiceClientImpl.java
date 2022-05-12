a5654a4b4d6ffc4510d3ca9331f26e19cb95a3d0
NaGaaDEV <NaGaaDEV@gmail.com>
2022-05-11 21:44:51 -0500

    spring cloud - client, server, eureka with circuitBreaker

--- spring-boot/exercise-16-cloud/client/src/main/java/ea/lab/exercise_16/client/service/CountryServiceClientImpl.java
+++ spring-boot/exercise-16-cloud/client/src/main/java/ea/lab/exercise_16/client/service/CountryServiceClientImpl.java
@@ -1,5 +1,7 @@
 package ea.lab.exercise_16.client.service;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import ea.lab.exercise_16.client.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,19 +22,38 @@ public class CountryServiceClientImpl implements CountryServiceClient {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
     @Value("${spring-boot-server.name}")
     private String serverName;
 
     @Override
     public List<Country> getCountries() {
-        String url = getBaseServiceUrl() + "/countries";
-        return Arrays.asList(restTemplate.getForObject(url, Country[].class));
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitBreaker");
+        return circuitBreaker.run(
+                () -> Arrays.asList(restTemplate.getForObject(getBaseServiceUrl() + "/countries", Country[].class)),
+                throwable -> getCountriesFallBack()
+        );
     }
 
     @Override
     public Country getCountry(Integer id) {
-        String url = getBaseServiceUrl() + "/countries/" + id;
-        return restTemplate.getForObject(url, Country.class);
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitBreaker");
+        return circuitBreaker.run(
+                () -> restTemplate.getForObject(getBaseServiceUrl() + "/countries/" + id, Country.class),
+                throwable -> getCountryFallBack()
+        );
+    }
+
+    private List<Country> getCountriesFallBack() {
+        System.out.println("Inside getCountries fallback!");
+        return new ArrayList<>();
+    }
+
+    private Country getCountryFallBack() {
+        System.out.println("Inside getCountry fallback!");
+        return null;
     }
 
     private String getBaseServiceUrl() {
